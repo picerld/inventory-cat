67ae7c39165456d7bc6bac28ab9f4994a1495562
picerld <155806349+picerld@users.noreply.github.com>
2025-11-07 17:21:10 +0700

    [auth] feat: middleware using protectedProcedure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { type AppType } from "next/app";
-import { Geist } from "next/font/google";
-
+import { Poppins } from "next/font/google";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ThemeProvider } from "~/components/ui/theme-provider";
 import { Toaster } from "~/components/ui/sonner";
 
-const geist = Geist({
+const poppins = Poppins({
   subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
@@ -19,7 +21,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       enableSystem
       disableTransitionOnChange
     >
-      <div className={geist.className}>
+      <div className={poppins.className}>
         <Component {...pageProps} />
         <Toaster />
       </div>
