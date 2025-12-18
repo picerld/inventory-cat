80e2d9ad17a46ce19c8da1d7bb4c79d881f66ef2
picerld <155806349+picerld@users.noreply.github.com>
2025-12-18 09:13:35 +0700

    base url

--- src/pages/qr/semi-finished/[id].tsx
+++ src/pages/qr/semi-finished/[id].tsx
@@ -38,6 +38,7 @@ export default function SemiFinishedGoodQrPage({ item }: QRPageProps) {
     process.env.NEXT_PUBLIC_APP_URL || process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:3000";
+
   const qrRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
